4320eb6aeb3b62bf779e363a00581351a76e8ad1
DaryaSerikova <dvserikova@gmail.com>
2022-01-12 01:22:58 +0700

    interface and nice console.log

--- index.ts
+++ index.ts
@@ -1,6 +1,22 @@
 import axios from 'axios';
 
 const url ='https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 axios.get(url).then(response => {
-    console.log(response.data);
+    const todo = response.data as Todo;
+    // console.log(response.data);
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    console.log(`
+    Todo с id = ${id} и текстом "${title}". 
+    Завершен? ${completed}
+    `)
 });
